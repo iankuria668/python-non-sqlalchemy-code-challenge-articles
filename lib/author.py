5a69d18310cab556c792f5e4ad9acd61f92b027e
iankuria668 <iankuria668@gmail.com>
2024-06-03 08:26:55 +0300

    Author created

---
+++ lib/author.py
@@ -0,0 +1,11 @@
+class Author:
+    def __init__(self,name):
+        if not isinstance(name,str) or len(name) == 0:
+            raise ValueError('Name should be entered')
+        self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+author1 = Author()
