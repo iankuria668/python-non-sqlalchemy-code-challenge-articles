09c688c0f28ff31ad329eff97eb49c169e677a87
iankuria668 <iankuria668@gmail.com>
2024-06-03 21:05:30 +0300

    @property added to article

--- lib/article.py
+++ lib/article.py
@@ -3,5 +3,13 @@ class Article:
         if not isinstance(title,str) or not(5<=len<=50):
             raise ValueError('Name should be entered')
         self.title = title
+
+        @property
+        def author (self):
+            return self._author
+        
+        @property
+        def magazine(self):
+            return  self._magazine
     
     
