7641cd6c1ba87ae6a1b5ddee497d53515ee7e1c2
iankuria668 <iankuria668@gmail.com>
2024-06-03 21:03:24 +0300

    Top publisher changes made to Magazine.py

--- lib/magazine.py
+++ lib/magazine.py
@@ -1,4 +1,6 @@
 class Magazine:
+    all_magazines = []
+
     def __init__(self,name,category):
         if not isinstance(name,str) or (2<=len(name) <= 0):
             raise ValueError('Name should be entered')
@@ -12,4 +14,29 @@ class Magazine:
     def name(self):
         return self._name
     
+    @property
+    def category(self):
+        return self._category
     
+    def add_articles(self,article):
+        self._articles.append(article)
+
+    def articles(self):
+        return self.add_articles
+    
+    def contributors(self):
+        return list({article.author for article in self._articles})
+    
+    def contributing_authors(self):
+        if not self._article:
+            return None
+        count  = {}
+        for article in self._articles:
+            count[article.author] = count.get(article.author, 0) + 1
+            return [author for author, x in  count.items() if x > 2]
+    
+    @classmethod
+    def top_publisher(cls):
+        if not cls._all_magazines:
+            return None
+        return cls._all_magazines
